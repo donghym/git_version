77c68f26d6c1a07a7b475d6639e3a36312d436e2
donghym <donghym@yonyou.com>
2022-04-01 13:32:57 +0800

    start a new project

---
+++ main.js
@@ -0,0 +1,58 @@
+/*
+ * @Author: donghym
+ * @Date: 2022-04-01 12:44:25
+ * @LastEditors: donghym
+ * @Description: 
+ * @LastEditTime: 2022-04-01 12:44:25
+ * @FilePath: /git_version/main.js
+ */
+const child_process = require('child_process');
+
+// git 最后一次提交的 Head
+const commit = child_process
+  .execSync('git show -s --format=%H')
+  .toString()
+  .trim();
+const branch = child_process
+  .execSync('git rev-parse --abbrev-ref HEAD')
+  .toString()
+  .trim();
+const commitUserName = child_process
+  .execSync('git show -s --format=%cn')
+  .toString()
+  .trim();
+const commitUserMail = child_process
+  .execSync('git show -s --format=%ce')
+  .toString()
+  .trim();
+
+const commitDateObj = new Date(child_process.execSync(`git show -s --format=%cd`).toString());
+const commitDate = `${commitDateObj.getFullYear() +
+  '-' +
+  (commitDateObj.getMonth() + 1) +
+  '-' +
+  commitDateObj.getDate() +
+  ' ' +
+  commitDateObj.getHours() +
+  ':' +
+  commitDateObj.getMinutes()}`;
+// const buildUserName = child_process
+//     .execSync('git config user?.name')
+//     .toString()
+//     .trim();
+// const buildUserMail = child_process
+//     .execSync('git config user?.email')
+//     .toString()
+//     .trim();
+const nowDate = new Date();
+const buildDate = `${nowDate.getFullYear() +
+  '-' +
+  (nowDate.getMonth() + 1) +
+  '-' +
+  nowDate.getDate() +
+  ' ' +
+  nowDate.getHours() +
+  ':' +
+  nowDate.getMinutes()}`;
+
+module.exports = { commit,branch, commitUserName, commitUserMail, commitDate, buildDate };
